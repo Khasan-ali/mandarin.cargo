2054373a3120e1216e945e4ca2336b2f2a94d636
KeldiyorDev <keldiyorraxmatov1303@gmail.com>
2024-01-05 22:53:10 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+
+  // fetch('http//http://localhost:9090/teacher/getById/2')
+  // .then(res => res.json())
+  // .then(response => console.log(response))
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
